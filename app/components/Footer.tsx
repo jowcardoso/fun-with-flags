eb4fcb5b4bef86e34e8afa23f8d40ce615d6318d
jowcardoso <jow.card@gmail.com>
2024-12-06 15:27:45 -0300

    add footer and header components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="py-6 mb-8">
+            <p className="text-center text-sm">Made with ❤️ by <a href="https://github.com/jowcardoso">Jonathan Cardoso</a></p>
+        </footer>
+    );
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Header, Footer } from "./components";
+
 export default function Home() {
   return (
     <>
-      <header> This is a header </header>
+      <Header />
       <main className="flex-1"> This is a main </main>
-      <footer> This is a footer </footer>
+      <Footer />
     </>
   );
 }
